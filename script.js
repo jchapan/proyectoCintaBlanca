300ad4c2ed9a2d19cbdb2079b40d40ea66e58eb2
jchapan <jchapan@gmail.com>
2020-06-05 17:00:05 -0500

    Funcion Tabla

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-aun no hay nada probando subir a git
+var i=0
+var txt
+var fila
+var siguiente
+
+var gastos =[
+    {id:1, desc:"Gas", cant:"1000", cat:"Servicios", rec:"Mensual"},
+    {id:2, desc:"Colegiatura", cant:"20000", cat:"Colegiatura", rec:"Mensual"},
+    {id:3, desc:"Super", cant:"2000", cat:"Consumos", rec:"Semanal"}
+]
+
+function extra(){
+    gastos.forEach(i => {
+        console.log(i);
+        var a = i.id
+        var b = i.desc
+        var c = "$" + new Intl.NumberFormat("en-US").format(i.cant)
+        var d = i.cat
+        var e = i.rec
+        console.log(a, b, c, d, e)
+        fila = `<tr><th> ${a}</th><td> ${b}</td><td> ${c}</td><td> ${d}</td><td> ${e}</td></tr>`
+        var btn = document.createElement("TR");
+        btn.innerHTML=fila;        
+        document.getElementById("tablaGastos").appendChild(btn);
+    });
+}
